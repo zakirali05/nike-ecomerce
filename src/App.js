a56bf11984738ae37b7568f12406d7e560bcf6d1
zakirali05 <109236535+zakirali05@users.noreply.github.com>
2022-12-08 12:01:08 +0530

    app done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,16 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Nav";
 import Popular from "./components/Popular";
 import High from "./components/High";
-import { toprateslaes, popularsales, sneaker, highlight } from "./data/data";
+import Stories from "./components/Stories";
+import Footer from "./components/Footer";
+import {
+  toprateslaes,
+  popularsales,
+  sneaker,
+  highlight,
+  story,
+  footerAPI,
+} from "./data/data";
 function App() {
   return (
     <div className="App flex flex-col gap-12">
@@ -12,6 +21,8 @@ function App() {
       <High d={highlight} />
       <Popular array={toprateslaes} isp={false} />
       <High d={sneaker} />
+      <Stories story={story} />
+      <Footer footerAPI={footerAPI} />
     </div>
   );
 }
---
+++ src/utils/Title.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Title = ({ title }) => {
+  return (
+    <>
+      <div className="grid items-center">
+        <h1
+          className="text-4xl lg:text-4xl md:text-3xl font-bold text-slate-900 filter
+         drop-shadow-lg"
+        >
+          {title}
+        </h1>
+      </div>
+    </>
+  );
+};
+
+export default Title;
